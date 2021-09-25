f91c61de81decd7f6a6e157fc7d1c66095e007b9
codie <codiechanel@gmail.com>
2021-09-25 15:36:33 +0800

    init

---
+++ packages/web/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Button } from "@codiechanel/common-demo";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Button textColor="yellow">Hello, click me!</Button>
+    </div>
+  );
+}
+
+export default App;
